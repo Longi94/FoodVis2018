f70c703cf5679151836d2f955169278784f0da88
Long Tran <lngtrn94@gmail.com>
2018-03-04 21:38:37 +0100

    autocomplete search (only prints to console)

--- FoodVisFrontend/js/index.js
+++ FoodVisFrontend/js/index.js
@@ -1,4 +1,4 @@
-for(var j = 1; j < 5; j++){    
+for(var j = 1; j < 5; j++){
     var getData = function(){
       var size = 3;
       var data = {};
@@ -15,4 +15,27 @@ for(var j = 1; j < 5; j++){
                   .$el(d3.select("#donut"+j))
                   .data(getData())
                   .render();
+}
+
+$.get('json/categories.json', function (categories) {
+    $("#search-input").autocomplete({
+        source: categories,
+        minLength: 3
+    })
+}).fail(function (xhr, error) {
+    console.log(error)
+});
+
+function search() {
+    var query = $("#search-input").val();
+
+    query = query.split(" ").join("-");
+
+    $.get(
+        "http://localhost:3000/api/Products/search",
+        {category: query, limit: 50},
+        function (result) {
+            console.log(result);
+        }
+    );
 }
