a944cc9b59360ffc5396764af3dcea51674d306c
Long Tran <lngtrn94@gmail.com>
2018-03-11 13:10:45 +0100

    ecmascript6 barchart

--- FoodVisFrontend/js/norm-bar.js
+++ FoodVisFrontend/js/norm-bar.js
@@ -62,152 +62,136 @@ const selectedIngredients = [
     "id"
 ];
 
-(function(window) {
-    var barTooltip = d3.select("body").append("div")
-        .attr("class", "bar-tooltip")
-        .style("opacity", "0");
+let barTooltip = d3.select("body").append("div")
+    .attr("class", "bar-tooltip")
+    .style("opacity", "0");
 
 
-    var currentMousePos = {x: -1, y: -1};
-    $(document).mousemove(function (event) {
-        currentMousePos.x = event.pageX;
-        currentMousePos.y = event.pageY;
-        barTooltip
-            .style("left", currentMousePos.x - 100 + "px")
-            .style("top", currentMousePos.y - 65 + "px");
+let currentMousePos = {x: -1, y: -1};
+$(document).mousemove(event => {
+    currentMousePos.x = event.pageX;
+    currentMousePos.y = event.pageY;
+    barTooltip
+        .style("left", currentMousePos.x - 100 + "px")
+        .style("top", currentMousePos.y - 65 + "px");
+});
+
+let svg = d3.select("#norm-bar > svg"),
+    margin = {top: 20, right: 60, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+let x = d3.scaleBand()
+    .rangeRound([0, width])
+    .padding(0.1)
+    .align(0.1);
+
+let y = d3.scaleLinear()
+    .rangeRound([height, 0]);
+
+let z = d3.scaleOrdinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+let stack = d3.stack()
+    .offset(d3.stackOffsetExpand);
+
+window.setBarChartData = function (products) {
+    if (products.length === 0) {
+        return;
+    }
+    Object.keys(products[0]).forEach(key => {
+        let removeKey = true;
+
+        if ($.inArray(key, selectedIngredients) !== -1) {
+            for (let i = 0; i < products.length; i++) {
+                if (products[i][key] !== "") {
+                    removeKey = false;
+                    break;
+                }
+            }
+        }
+
+        if (removeKey) {
+            products.forEach(product => {
+                delete product[key];
+            });
+        }
     });
 
-    var svg = d3.select("#norm-bar > svg"),
-        margin = {top: 20, right: 60, bottom: 30, left: 40},
-        width = +svg.attr("width") - margin.left - margin.right,
-        height = +svg.attr("height") - margin.top - margin.bottom,
-        g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var x = d3.scaleBand()
-        .rangeRound([0, width])
-        .padding(0.1)
-        .align(0.1);
-
-    var y = d3.scaleLinear()
-        .rangeRound([height, 0]);
-
-    var z = d3.scaleOrdinal()
-        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-    var stack = d3.stack()
-        .offset(d3.stackOffsetExpand);
-
-    window.setBarChartData = function(products) {
-        if (products.length === 0) {
-            return;
-        }
-        Object.keys(products[0]).forEach(function (key) {
-            var removeKey = true;
-
-            if ($.inArray(key, selectedIngredients) !== -1) {
-                for (var i = 0; i < products.length; i++) {
-                    if (products[i][key] !== "") {
-                        removeKey = false;
-                        break;
-                    }
-                }
-            }
-
-            if (removeKey) {
-                products.forEach(function (product) {
-                    delete product[key];
-                });
+    products.forEach(product => {
+        Object.keys(product).forEach(key => {
+            if (typeof product[key] !== "number" && key !== "id" && key !== "product_name") {
+                product[key] = 0;
             }
         });
+    });
 
-        products.forEach(function (product) {
-            Object.keys(product).forEach(function (key) {
-                if (typeof product[key] !== "number" && key !== "id" && key !== "product_name") {
-                    product[key] = 0
-                }
+    /*const sortKey = Object.keys(products[0])[0];
+
+    products.sort(function (a, b) {
+        return a[sortKey] - b[sortKey]
+    });*/
+
+    let keys = Object.keys(products[0]).filter(k => k !== "id" && k !== "product_name");
+
+    x.domain(products.map(d => d.id));
+    z.domain(keys);
+
+    g.html("");
+
+    let serie = g.selectAll(".serie")
+        .data(stack.keys(keys)(products))
+        .enter().append("g")
+        .attr("class", "serie")
+        .attr("fill", d => z(d.key));
+
+    serie.selectAll("rect")
+        .data(d => {
+            d.forEach(value => {
+                value.key = d.key;
             });
+            return d;
+        })
+        .enter().append("rect")
+        .attr("x", d => x(d.data.id))
+        .attr("y", d => y(d[1]))
+        .attr("height", d => y(d[0]) - y(d[1]))
+        .attr("width", x.bandwidth())
+        .on("mouseover", function (d) {
+            barTooltip.style("opacity", .75);
+            barTooltip.html("<span>" + d.data.product_name + "</span>" + "<span>" + d.key + " - " + d.data[d.key] + "</span>");
+        })
+        .on("mouseout", () => {
+            barTooltip.style("opacity", 0);
         });
 
-        /*const sortKey = Object.keys(products[0])[0];
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .call(d3.axisLeft(y).ticks(10, "%"));
 
-        products.sort(function (a, b) {
-            return a[sortKey] - b[sortKey]
-        });*/
-
-        var keys = Object.keys(products[0]).filter(function (k) {
-            return k !== "id" && k !== "product_name"
+    let legend = serie.append("g")
+        .attr("class", "legend")
+        .attr("transform", d => {
+            let d2 = d[d.length - 1];
+            return "translate(" + (x(d2.data.id) + x.bandwidth()) + "," + ((y(d2[0]) + y(d2[1])) / 2) + ")";
         });
 
-        x.domain(products.map(function (d) {
-            return d.id
-        }));
-        z.domain(keys);
+    legend.append("line")
+        .attr("x1", -6)
+        .attr("x2", 6)
+        .attr("stroke", "#000");
 
-        g.html("");
+    legend.append("text")
+        .attr("x", 9)
+        .attr("dy", "0.35em")
+        .attr("fill", "#000")
+        .style("font", "10px sans-serif")
+        .text(d => d.key);
+};
 
-        var serie = g.selectAll(".serie")
-            .data(stack.keys(keys)(products))
-            .enter().append("g")
-            .attr("class", "serie")
-            .attr("fill", function (d) {
-                return z(d.key);
-            });
-
-        serie.selectAll("rect")
-            .data(function (d) {
-                d.forEach(function (value) {
-                    value.key = d.key;
-                });
-                return d;
-            })
-            .enter().append("rect")
-            .attr("x", function (d) {
-                return x(d.data.id);
-            })
-            .attr("y", function (d) {
-                return y(d[1]);
-            })
-            .attr("height", function (d) {
-                return y(d[0]) - y(d[1]);
-            })
-            .attr("width", x.bandwidth())
-            .on("mouseover", function (d) {
-                barTooltip.style("opacity", .75);
-                barTooltip.html("<span>" + d.data.product_name + "</span>" + "<span>" + d.key + " - " + d.data[d.key] + "</span>");
-            })
-            .on("mouseout", function (d) {
-                barTooltip.style("opacity", 0);
-            });
-
-        g.append("g")
-            .attr("class", "axis axis--y")
-            .call(d3.axisLeft(y).ticks(10, "%"));
-
-        var legend = serie.append("g")
-            .attr("class", "legend")
-            .attr("transform", function (d) {
-                var d = d[d.length - 1];
-                return "translate(" + (x(d.data.id) + x.bandwidth()) + "," + ((y(d[0]) + y(d[1])) / 2) + ")";
-            });
-
-        legend.append("line")
-            .attr("x1", -6)
-            .attr("x2", 6)
-            .attr("stroke", "#000");
-
-        legend.append("text")
-            .attr("x", 9)
-            .attr("dy", "0.35em")
-            .attr("fill", "#000")
-            .style("font", "10px sans-serif")
-            .text(function (d) {
-                return d.key;
-            });
-    }
-
-    function type(d, i, columns) {
-        for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
-        d.total = t;
-        return d;
-    }
-})(window);
+function type(d, i, columns) {
+    for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
+    d.total = t;
+    return d;
+}
