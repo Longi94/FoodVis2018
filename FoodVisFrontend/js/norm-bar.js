b13ba9751fc0d940d8ae5f4c66dc89bbd8626ff9
Long Tran <lngtrn94@gmail.com>
2018-03-11 21:23:41 +0100

    reorder barchart

--- FoodVisFrontend/js/norm-bar.js
+++ FoodVisFrontend/js/norm-bar.js
@@ -1,6 +1,6 @@
-const selectedIngredients = [
+let selectedIngredients = [
     "fat_100g",
-    "carbohydrates_100g",
+    //"carbohydrates_100g",
     "sugars_100g",
     "fiber_100g",
     "proteins_100g",
@@ -12,8 +12,8 @@ const colors = {
     carbohydrates_100g: "#D95B43",
     sugars_100g: "#C02942",
     fiber_100g: "#542437",
-    proteins_100g: "#53777A",
-    salt_100g: "#53777A",
+    proteins_100g: "#425b94",
+    salt_100g: "#45965b"
 };
 
 let barTooltip = d3.select("body").append("div")
@@ -44,15 +44,16 @@ let x = d3.scaleBand()
 let y = d3.scaleLinear()
     .rangeRound([height, 0]);
 
-let stack = d3.stack()
-    .offset(d3.stackOffsetExpand);
+let stack = d3.stack();
+
+let data;
 
 function setBarChartData(products) {
     if (products.length === 0) {
         return;
     }
 
-    let filtered = products.filter(product => {
+    data = products.filter(product => {
         for (let i = 0; i < selectedIngredients.length; i++) {
             if (product[selectedIngredients[i]] > 0) {
                 return true;
@@ -61,14 +62,12 @@ function setBarChartData(products) {
         return false;
     });
 
-    filtered.sort((a, b) => a.fat_100g - b.fat_100g);
-
-    x.domain(filtered.map(d => d.id));
+    x.domain(data.map(d => d.id));
 
     g.html("");
 
     let serie = g.selectAll(".serie")
-        .data(stack.keys(selectedIngredients)(filtered))
+        .data(stack.keys(selectedIngredients)(data))
         .enter().append("g")
         .attr("class", "serie")
         .attr("fill", d => colors[d.key]);
@@ -81,18 +80,19 @@ function setBarChartData(products) {
             return d;
         })
         .enter().append("rect")
+        .attr("id", d => "bar-" + d.data.id + "-" + d.key)
         .attr("x", d => x(d.data.id))
-        .attr("y", d => y(d[1]))
-        .attr("height", d => y(d[0]) - y(d[1]))
+        .attr("y", d => y(d[1] / 100))
+        .attr("height", d => y(d[0] / 100) - y(d[1] / 100))
         .attr("width", x.bandwidth())
         .on("mouseover", d => {
             barTooltip.style("opacity", .75);
-            barTooltip.html("<span>" + d.data.product_name + "</span>" + "<span>" + d.key + " - " + d.data[d.key] + "</span>");
+            barTooltip.html("<span>" + d.data.product_name + "</span><span>" + d.key + " - " + d.data[d.key] + "</span>");
         })
         .on("mouseout", () => {
             barTooltip.style("opacity", 0);
         })
-        .on("click", d => console.log(d));
+        .on("click", d => selectBar(d.key));
 
     g.append("g")
         .attr("class", "axis axis--y")
@@ -100,9 +100,10 @@ function setBarChartData(products) {
 
     let legend = serie.append("g")
         .attr("class", "legend")
+        .attr("id", d => "legend-" + d[d.length - 1].key)
         .attr("transform", d => {
             let d2 = d[d.length - 1];
-            return "translate(" + (x(d2.data.id) + x.bandwidth()) + "," + ((y(d2[0]) + y(d2[1])) / 2) + ")";
+            return "translate(" + (x(d2.data.id) + x.bandwidth()) + "," + ((y(d2[0] / 100) + y(d2[1] / 100)) / 2) + ")";
         });
 
     legend.append("line")
@@ -116,10 +117,37 @@ function setBarChartData(products) {
         .attr("fill", "#000")
         .style("font", "10px sans-serif")
         .text(d => d.key);
-};
+}
 
-function type(d, i, columns) {
-    for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
-    d.total = t;
-    return d;
+function selectBar(key) {
+    let index = selectedIngredients.indexOf(key);
+    selectedIngredients.splice(index, 1);
+    selectedIngredients.unshift(key);
+
+    data.sort((a, b) => a[key] - b[key]);
+    x.domain(data.map(d => d.id));
+
+    y = d3.scaleLinear()
+        .rangeRound([height, 0]);
+
+    let newStack = d3.stack()
+        .keys(selectedIngredients)(data);
+
+    let trans = d3.transition()
+        .duration(600)
+        .ease(d3.easeExpOut);
+
+    newStack.forEach(k => {
+        k.forEach(d => {
+            d3.select("#bar-" + d.data.id + "-" + k.key)
+                .transition(trans)
+                .attr("x", x(d.data.id))
+                .attr("y", y(d[1] / 100));
+        });
+
+        let d2 = k[k.length - 1];
+        d3.select("#legend-" + k.key)
+            .transition(trans)
+            .attr("transform", "translate(" + (x(d2.data.id) + x.bandwidth()) + "," + ((y(d2[0] / 100) + y(d2[1] / 100)) / 2) + ")");
+    });
 }
